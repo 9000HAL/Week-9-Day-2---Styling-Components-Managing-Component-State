d7905daafcafb46882ef464662e8f669d73f373b
9000HAL <gsanchezscs@gmail.com>
2023-06-22 04:26:20 -0700

    setting up for like button

---
+++ app/src/App.tsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import React from 'react'mkdir my-like-button
+
+const App = () => {
+  //JavaScript XML
+  return (
+    <div>Hello, Thieves</div>
+  )
+}
+
+export default App
+
+*/
